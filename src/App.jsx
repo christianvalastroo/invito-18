f2971b7dcfc1e34120554b60c7a8bf56ecd06bfe
Christian Valastro <christianvalastro@icloud.com>
2026-08-18 00:38:45 +0200

    feat: add invitation scene background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 
 function App() {
-  return <main className="app" />
+  return (
+    <main className="invitation-scene">
+      <div className="scene-overlay" aria-hidden="true" />
+    </main>
+  )
 }
 
 export default App
